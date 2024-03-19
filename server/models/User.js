6ecebcefd1d1abbe9c415fd366ba03dada285ea8
mohammad hossein mosaleh <mosaleh.com@gmail.com>
2024-03-19 05:09:39 +0330

    new user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,46 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true },
+    avatar: { type: String },
+    email: { type: String, lowercase: true, trim: true },
+    phoneNumber: { type: String, trim: true },
+    password: { type: String },
+    resetLink: { type: String, default: null },
+    isVerifiedPhoneNumber: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: false },
+    status: { type: Number, required: true, default: 1, enum: [0, 1, 2] }, // 0, 1, 2
+    role: { type: String, default: "USER" },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// UserSchema.virtual("avatarUrl").get(function () {
+//   if (this.avatar) return `${process.env.SERVER_URL}/${this.avatar}`;
+//   return null;
+// });
+
+UserSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.avatar;
+  return obj;
+};
+
+UserSchema.index({
+  name: "text",
+  email: "text",
+  phoneNumber: "text",
+  username: "text",
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
