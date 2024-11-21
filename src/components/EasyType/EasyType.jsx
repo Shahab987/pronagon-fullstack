d5385874c71a4d1021f6768c811bc140f9499eb4
Shahab Mosaleh <mosaleh.com@gmail.com>
2024-11-21 21:50:32 +0330

    keyboard type 2

--- src/components/EasyType/EasyType.jsx
+++ src/components/EasyType/EasyType.jsx
@@ -197,6 +197,7 @@ const EasyType = () => {
   const [menuWidth, setMenuWidth] = useState(312);
   const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
   const [predictions, setPredictions] = useState([]);
+  const [keyboardType, setKeyboardType] = useState(0)
 
   const handleMouseMove = (e) => {
     const x = e.clientX;
@@ -273,16 +274,17 @@ const EasyType = () => {
       className="flex flex-col h-125 cursor-none"
       onClick={() => setMenuVisible(false)} // Close menu on outside click
     >
+      {/* ------------------------- Green Cursor -------------------------- */}
       <div
         className="bg-lime-500 absolute w-7 h-7 cursor-none z-40 drop-shadow-md border-2 border-lime-800"
         style={{
           top: position.y,
           left: position.x,
           borderRadius: "10% 50% 50% 50%",
-          pointerEvents: "none", // So it doesn't interfere with the cursor movement
+          pointerEvents: "none", 
         }}
       ></div>
-
+      {/* ------------------------ Text area ----------------------------- */}
       <div className="flex items-center justify-center border border-gray-300">
         <textarea
           value={text}
@@ -298,7 +300,9 @@ const EasyType = () => {
           placeholder="---"
         />
       </div>
-      <div className="min-h-12">
+      {/* ----------------------- predictions ---------------------- */}
+      <div className="min-h-12" style={keyboardType === 1 ? {display:"block"} : {display:"none"}}>
+        
         <div className="flex flex-row-reverse text-3xl font-VazirMatn gap-5 mt-1">
         {predictions.map((word, index) => (
           <button
@@ -310,11 +314,49 @@ const EasyType = () => {
           </button>
         ))}
         </div>
+        <button
+          onClick={() => setKeyboardType(p=>p===1?0:1)}
+          className="px-3 py-2 text-gray-700 bg-green-100 hover:bg-gray-200 rounded border cursor-none w-1/6"
+        >
+          مدل {keyboardType ? "جدید" : "قدیم"}
+        </button>
+      </div>
+      {/* ----------------------- Long Space and back space ---------------------- */}
+      <div className="min-h-16" style={keyboardType === 0 ? {display:"block"} : {display:"none"}}>
+        <div className="flex flex-row-reverse text-4xl font-IRYekan gap-2 mt-1 ">
+        <button
+          onClick={() => handleMenuOptionClick("backspace")}
+          className="px-3 py-2 text-gray-700 bg-yellow-100 hover:bg-gray-200 rounded border cursor-none w-1/3"
+        >
+          حذف
+        </button>
+        <button
+          onClick={() => handleMenuOptionClick("space")}
+          className="px-3 py-2 text-gray-700 bg-blue-100 hover:bg-gray-200 rounded border cursor-none w-1/3"
+        >
+          فاصله
+        </button>
+        <button
+          onClick={() => handleMenuOptionClick("clear")}
+          className="px-3 py-2 text-gray-700 bg-red-100 hover:bg-gray-200 rounded border cursor-none w-1/3"
+        >
+          پاک
+        </button>
+        <button
+          onClick={() => setKeyboardType(p=>p===1?0:1)}
+          className="px-3 py-2 text-xl text-gray-700 bg-green-100 hover:bg-gray-200 rounded border cursor-none w-1/6"
+        >
+           مدل {keyboardType ? "جدید" : "قدیم"}
+        </button>
+        
+        </div>
       </div>
 
+      {/* -------------- horizontal keyboard -----------------  */}
       <div
         onContextMenu={handleRightClick}
         className="flex flex-nowrap flex-row-reverse h-full border mt-1 relative cursor-none"
+        style={keyboardType === 1 ? {display:"flex"} : {display:"none"}}
       >
         {farsiLetters.map((letter, index) => (
           <div
@@ -344,7 +386,43 @@ const EasyType = () => {
           </div>
         ))}
       </div>
+      
+      {/* -------------- Complex keyboard -----------------  */}
+      <div
+        onContextMenu={handleRightClick}
+        className="flex flex-wrap flex-row-reverse border mt-1 relative cursor-none"
+        style={keyboardType === 0 ? {display:"flex"} : {display:"none"}}
+      >
+        {farsiLetters.map((letter, index) => (
+          <div
+            key={index}
+            onClick={() => handleLetterClick(letter)}
+            onMouseEnter={() => setHoveredLetter(letter)}
+            onMouseLeave={() => setHoveredLetter(null)}
+            className={`relative font-IRYekan text-xl xl:text-5xl lg:text-3xl text-center cursor-none p-2
+              border border-gray-400 w-[6.25%] h-45 rounded  hover:border-black hover:bg-slate-400 
+              ${index % 2 === 0 ? "bg-slate-50" : "bg-slate-200"}`}
+          >
+            {letter}
 
+            <div
+              className={`z-1 absolute top-26 flex justify-center items-center pointer-events-none ${
+                index > 28 ? "left-14" : "right-14"
+              } ${
+                hoveredLetter === letter ? "opacity-100" : "opacity-0"
+              } text-8xl font-VazirMatn font-bold text-pink-900 p-1 border-2 border-pink-800 shadow-6 rounded-xl bg-gray-100 w-30 h-30 duration-100`}
+            >
+              {hoveredLetter === " " ? (
+                <p className="text-5xl">فاصله</p>
+              ) : (
+                hoveredLetter
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+
+        {/* --------------------- Rightclick menu --------------------- */}
       <div
         onContextMenu={(e) => {
           e.preventDefault();
